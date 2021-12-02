e3d7aaf3c5e71fe39550829ab08048f2a73f0519
rasul-surname <rasul.surname@mail.ru>
2021-12-02 16:07:41 +0300

    Добавил компонент формы из Ant Design

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import React from "react";
+import FormComponent from "../client/components/ui/Form/Form";
+
 const Home = () => {
   return (
-    <div></div>
+    <>
+      <FormComponent />
+    </>
   )
 }
 
